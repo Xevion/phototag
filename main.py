c9ba6add63a945814815b473a0012f7c29327fbf
Xevion <ryanchwalters@gmail.com>
2019-08-07 23:54:31 -0500

    restructure

--- main.py
+++ main.py
@@ -1,23 +1,5 @@
-import io, sys, os
 
-# Imports the Google Cloud client library
-from google.cloud import vision
-from google.cloud.vision import types
-
-# Instantiates a client
-client = vision.ImageAnnotatorClient()
-
-# The name of the image file to annotate
-path = os.path.join(sys.path[0], 'DSC_6315.jpg')
-
-# Loads the image into memory
-with io.open(path, 'rb') as image_file:
-    content = image_file.read()
-
-image = types.Image(content=content)
-
-# Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
-
-print([label.description for label in labels])
+if __name__ == "__main__":
+    import os
+    from package.app import run
+    os.exit(app.run())
---
+++ package/app.py
@@ -0,0 +1,26 @@
+import io, sys, os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+def run():
+
+    # The name of the image file to annotate
+    path = os.path.join(sys.path[0], 'DSC_6315.jpg')
+    print(path)
+
+    # Loads the image into memory
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    print([label.description for label in labels])
